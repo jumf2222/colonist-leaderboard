d62e70c31e9ff65e2812a4c60dbf6d5eeabef2d0
Julian de Rushe <julian.derushe@mail.utoronto.ca>
2022-09-25 22:59:04 -0400

    add exact match and limit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -20,20 +20,23 @@ export interface Leaderboard { games: number, players: Player[]; }
 
 export const load: PageServerLoad<{ leaderboard: Leaderboard | null; usernames: string; }> = async ({ url }) => {
     const usernames = url.searchParams.get("usernames") ?? "";
+    const limit = parseInt(url.searchParams.get("limit") ?? "");
+    const exact = (url.searchParams.get("exact") ?? "true") === "true";
+
     const playerUsernames = new Set(usernames.split(",").map(name => name.trim()).filter(Boolean));
 
-    if (playerUsernames.size === 0) return { leaderboard: null, usernames };
+    if (playerUsernames.size === 0) return { leaderboard: null, usernames, exact };
 
     const fetchCookie = makeFetchCookie(fetch);
 
     await fetchCookie("https://colonist.io");
     const res = await fetchCookie(`https://colonist.io/api/profile/${playerUsernames.values().next().value}/history`);
 
-    if (!res.ok) return { leaderboard: { games: 0, players: [] }, usernames };
+    if (!res.ok) return { leaderboard: { games: 0, players: [] }, usernames, exact };
 
     const rawData: Game[] = await res.json();
 
-    const data = rawData.filter((game) => {
+    const allData = rawData.filter((game) => {
         if (!game.finished) return false;
 
         const set = new Set();
@@ -41,7 +44,7 @@ export const load: PageServerLoad<{ leaderboard: Leaderboard | null; usernames: 
         for (const player of game.players) {
             set.add(player.username);
 
-            if (!playerUsernames.has(player.username)) {
+            if (exact && !playerUsernames.has(player.username)) {
                 return false;
             }
         }
@@ -54,6 +57,8 @@ export const load: PageServerLoad<{ leaderboard: Leaderboard | null; usernames: 
 
         return true;
     }).sort((a, b) => a.startTime - b.startTime);
+
+    const data = allData.slice(Math.max((isNaN(limit) || limit < 0) ? 0 : allData.length - limit, 0));
 
     const players: Record<string, Player> = {};
 
@@ -95,13 +100,20 @@ export const load: PageServerLoad<{ leaderboard: Leaderboard | null; usernames: 
         playerList[i].winPercent = data.length === 0 ? 0 : playerList[i].ranks[0] / data.length;
     }
 
-    return { leaderboard: { games: data.length, players: playerList }, usernames };
+    return { leaderboard: { games: data.length, players: playerList }, usernames, exact };
 };
 
 export const actions: Actions = {
     default: async ({ request }) => {
         const data = await request.formData();
         const usernames = data.get("usernames");
-        throw redirect(303, !usernames ? "/" : `/?${new URLSearchParams({ usernames: String(usernames) })}`);
+        const exact = data.get("exact");
+
+        const params = new URLSearchParams({
+            ...(usernames && { usernames: String(usernames) }),
+            ...(!exact && { exact: String(exact) })
+        }).toString();
+
+        throw redirect(303, params ? `/?${params}` : '/');
     }
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,20 +10,40 @@
 	import type { PageData } from './$types';
 
 	let usernames = savedStore('usernames', '');
+	let exact = savedStore('exact', true);
+
 	export let data: PageData;
 
 	$: if (data.usernames) $usernames = data.usernames;
+	$: if (!data.exact) $exact = data.exact;
 
 	onMount(() => {
-		if ($usernames && new URLSearchParams(window.location.search).get('usernames') !== $usernames) {
-			goto(`/?${new URLSearchParams({ usernames: $usernames })}`);
+		const params = new URLSearchParams(window.location.search);
+		if (
+			($usernames && params.get('usernames') !== $usernames) ||
+			(!$exact && params.get('exact') !== String($exact))
+		) {
+			search();
 		}
 	});
 
-	const onSubmit: SubmitFunction = ({ data, cancel }) => {
+	const search = () => {
+		const params = new URLSearchParams({
+			...($usernames && { usernames: $usernames }),
+			...(!$exact && { exact: String($exact) })
+		}).toString();
+
+		goto(params ? `/?${params}` : '/', { keepfocus: true });
+	};
+
+	const onSubmit: SubmitFunction = ({ cancel }) => {
 		cancel();
-		const usernames = data.get('usernames');
-		goto(!usernames ? '/' : `/?${new URLSearchParams({ usernames: String(usernames) })}`);
+		search();
+	};
+
+	const onExactChange = (event: Event) => {
+		$exact = (event.target as HTMLInputElement).checked;
+		search();
 	};
 
 	$: title = `Rankings for ${
@@ -69,16 +89,22 @@
 <main>
 	<h1 class="logo">Colonist Leaderboard</h1>
 
-	<form class="input" use:enhance={onSubmit} method="post">
-		<input
-			type="text"
-			placeholder="Enter a comma-separated list of players..."
-			bind:value={$usernames}
-			name="usernames"
-		/>
-		<button>
-			<AddPeople alt="Add people icon" />
-		</button>
+	<form class="form-wrapper" use:enhance={onSubmit} method="post">
+		<div class="input">
+			<input
+				type="text"
+				placeholder="Enter a comma-separated list of players..."
+				bind:value={$usernames}
+				name="usernames"
+			/>
+			<button>
+				<AddPeople alt="Add people icon" />
+			</button>
+		</div>
+		<label class="checkbox-label">
+			Exact Match
+			<input type="checkbox" checked={$exact} name="exact" on:change={onExactChange} />
+		</label>
 	</form>
 
 	{#if data.leaderboard}
@@ -128,17 +154,17 @@
 
 	.nav-link:focus-visible {
 		outline: none;
-		color: var(--font);
-		background-color: var(--nav-hover);
+		color: var(--blue-font);
+		background-color: var(--blue-background);
 	}
 
 	.nav-link:focus-visible :global(svg path) {
-		fill: var(--font);
+		fill: var(--blue-font);
 	}
 
 	.nav-link:hover {
-		color: var(--blue-font);
-		background-color: var(--blue-background);
+		color: var(--font);
+		background-color: var(--nav-hover);
 	}
 
 	.nav-link :global(svg path) {
@@ -151,7 +177,7 @@
 	}
 
 	.nav-link:hover :global(svg path) {
-		fill: var(--blue-font);
+		fill: var(--font);
 	}
 
 	.games-text {
@@ -180,18 +206,98 @@
 		justify-content: center;
 	}
 
+	.form-wrapper {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 24px;
+		gap: 12px;
+	}
+
+	.checkbox-label {
+		height: 50px;
+		padding: 0 18px;
+		border-radius: 500px;
+		cursor: pointer;
+		display: flex;
+		flex-direction: column-reverse;
+		justify-content: center;
+		align-items: center;
+		gap: 4px;
+		font-size: 12px;
+		white-space: nowrap;
+		color: var(--light-font);
+		background-color: var(--surface);
+		box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 8px 0px;
+		transition: background-color 0.2s;
+	}
+
+	[type='checkbox']:focus-visible {
+		outline: 2px solid var(--blue-font);
+	}
+
+	.checkbox-label:hover {
+		background-color: var(--surface-hover);
+	}
+
+	/* CHECKBOX */
+
+	[type='checkbox'] {
+		width: 16px;
+		height: 16px;
+		color: var(--blue-font);
+		vertical-align: middle;
+		-webkit-appearance: none;
+		background: none;
+		border: 0;
+		outline: 0;
+		flex-grow: 0;
+		border-radius: 50%;
+		background-color: var(--background);
+		transition: background-color 0.2s;
+		cursor: pointer;
+	}
+
+	/* Pseudo element for check styling */
+
+	[type='checkbox']::before {
+		content: '';
+		color: transparent;
+		display: block;
+		width: inherit;
+		height: inherit;
+		border-radius: inherit;
+		border: 0;
+		background-color: transparent;
+		background-size: contain;
+		box-shadow: inset 0 0 0 1px var(--nav-hover);
+	}
+
+	/* Checked */
+
+	[type='checkbox']:checked {
+		background-color: var(--checkbox-background);
+	}
+
+	[type='checkbox']:checked::before {
+		box-shadow: none;
+		background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E %3Cpath d='M15.88 8.29L10 14.17l-1.88-1.88a.996.996 0 1 0-1.41 1.41l2.59 2.59c.39.39 1.02.39 1.41 0L17.3 9.7a.996.996 0 0 0 0-1.41c-.39-.39-1.03-.39-1.42 0z' fill='%23fff'/%3E %3C/svg%3E");
+	}
+
 	.input {
+		flex: 1;
 		display: flex;
 		max-width: 500px;
+		min-width: 0;
 		height: 50px;
-		margin: auto;
-		margin-bottom: 24px;
 		background-color: var(--surface);
 		border-radius: 500px;
 		box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 8px 0px;
 	}
 
 	.input input {
+		min-width: 0;
 		font-size: 16px;
 		padding: 0 22px;
 		background-color: transparent;
@@ -228,7 +334,6 @@
 
 	.input button:focus-visible {
 		background-color: var(--font);
-		outline: 2px solid var(--blue-font);
 	}
 
 	.input button:hover {
