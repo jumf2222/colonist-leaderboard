849c479f5242f257680a0b65114b139184b6071a
Julian de Rushe <jumf2222@gmail.com>
2026-03-23 04:53:36 -0400

    revert vite plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { solidStart } from "@solidjs/start/config";
 import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
-import { defineConfig } from "vite-plus";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    staged: {
-        "*": "vp check --fix",
-    },
-    fmt: {
-        tabWidth: 4,
-    },
-    lint: { options: { typeAware: true, typeCheck: true } },
     plugins: [solidStart({ ssr: false }), nitro()],
-    server: {
-        port: 25565,
-    },
+    server: { port: 25565 },
 });
